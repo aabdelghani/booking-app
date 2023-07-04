c98453fe0acc01a2932ea25fbac235ab5c29adc0
aabdelghani <ahmedabdelghany15@gmail.com>
2023-07-04 14:24:15 +0300

    Added the username and var

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	var conf_name = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to %v Booking application\n", conf_name)
+	fmt.Printf("We have total tickets %v and %v are still remaining", conferenceTickets, conferenceTickets-remainingTickets)
+	var userName 
+	// ask user for their name 
+
+	userName = "Tom"
+	fmt.println(userName)
+
 }
